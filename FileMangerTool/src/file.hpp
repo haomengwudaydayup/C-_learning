1f3af3b92cd3038f107e5fd7a1dfd85dbc699972
haomengwudaydayup <49704773+haomengwudaydayup@users.noreply.github.com>
2020-05-08 22:02:35 +0800

    遍历文件接口

---
+++ FileMangerTool/src/file.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_set>
+#include<io.h>
+
+//搜索目录
+void SearhDirection(const std::string&, std::unordered_set<std::string>&);
+//删除文件
+void DeleteFile(const char*);
